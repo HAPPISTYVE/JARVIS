b30e4792dec83a952f91a80c48f071938b65b534
HAPPISTYVE <happychendjou@icloud.com>
2026-03-10 21:19:55 +0100

    Update and rename gemini_service.py to groq_service.py

---
+++ backend/groq_service.py
@@ -0,0 +1,24 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def ask_ai(message, history):
+
+    messages = []
+
+    for h in history:
+        messages.append({"role": "user", "content": h["user"]})
+        messages.append({"role": "assistant", "content": h["bot"]})
+
+    messages.append({"role": "user", "content": message})
+
+    chat = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=messages
+    )
+
+    return chat.choices[0].message.content

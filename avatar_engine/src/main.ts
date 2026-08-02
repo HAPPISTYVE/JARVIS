7372e2a7585ccebce133dbd3d9e12197ba9ba9ba
HAPPISTYVE <happychendjou@icloud.com>
2026-08-02 20:53:36 +0100

    Update main.ts

--- avatar_engine/src/main.ts
+++ avatar_engine/src/main.ts
@@ -1,7 +1,28 @@
-import { GaussianAvatar } from './gaussianAvatar';
+import { GaussianAvatar } from "./gaussianAvatar";
+import { connectWebSocket } from "./services/websocket";
 
-const div = document.getElementById('LAM_WebRender');
-const assetPath = './asset/arkit/p2-1.zip';
+const div = document.getElementById("LAM_WebRender") as HTMLDivElement;
+const assetPath = "./asset/arkit/p2-1.zip";
 
 const gaussianAvatar = new GaussianAvatar(div, assetPath);
+
 gaussianAvatar.start();
+
+// Connexion au backend
+connectWebSocket((data) => {
+  console.log("Message reçu :", data);
+
+  switch (data.type) {
+
+    case "blendshapes":
+      gaussianAvatar.updateBlendshapes(data.data);
+      break;
+
+    case "state":
+      gaussianAvatar.setChatState(data.state);
+      break;
+
+    default:
+      console.log("Message inconnu :", data);
+  }
+});
